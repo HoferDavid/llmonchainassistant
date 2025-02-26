d6fe23f69a2db613f3cac796ffe6d8ea5f02bd69
David Hofer <chassis.poppets0x@icloud.com>
2025-02-26 17:01:13 -0300

    feat: add createRun method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import { createAssistant } from "./openai/createAssistant";
 import { createThread } from "./openai/createThread";
+import { createRun } from "./openai/createRun";
 
 async function main() {
     const client = new OpenAI();
@@ -8,6 +9,7 @@ async function main() {
 
     const assistant = await createAssistant(client);
     const thread = await createThread(client, message);
+    const run = await createRun(client, thread, assistant.id);
 
     console.log('hello world');
 }
