0870295e4de18fc4cfd71ac342b1dc9b3da3d2f8
David Hofer <chassis.poppets0x@icloud.com>
2025-02-26 15:34:22 -0300

    feat: add openai base functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import OpenAI from "openai";
+import { createAssistant } from "./openai/createAssistant";
+import { createThread } from "./openai/createThread";
+
 async function main() {
+    const client = new OpenAI();
+    const message = "hello alt cunningham!"
+
+    const assistant = await createAssistant(client);
+    const thread = await createThread(client, message);
+
     console.log('hello world');
 }
 
