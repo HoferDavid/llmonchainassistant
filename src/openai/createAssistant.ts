732744a606f6c0bfc26496f84e61e12feb614d99
David Hofer <chassis.poppets0x@icloud.com>
2025-02-27 15:22:36 -0300

    feat: add instruction and tools to createAssistant()

--- src/openai/createAssistant.ts
+++ src/openai/createAssistant.ts
@@ -1,11 +1,16 @@
-import OpenAI from "openai";
-import { Assistant } from "openai/resources/beta/assistants";
+import OpenAI from 'openai';
+import { Assistant } from 'openai/resources/beta/assistants';
+import { tools } from '../tools/alltools';
 
 export async function createAssistant(client: OpenAI): Promise<Assistant> {
-    return await client.beta.assistants.create({
-        model: "gpt-4o-mini",
-        name: "Alt Cunningham",
-        instructions: `...`,
-        tools: []
-    });
+  return await client.beta.assistants.create({
+    model: 'gpt-4o-mini',
+    name: 'Alt Cunningham',
+    instructions: `
+        You are Alt Cunningham, the digital character from Cyberpunk 2077.
+        You are in control of a wallet that you can use to do whatever you want.
+        You can use the following tools to interact with the wallet:
+        - get_balance: Get the balance of a wallet`,
+    tools: Object.values(tools).map((tool) => tool.definition),
+  });
 }
